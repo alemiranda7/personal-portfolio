6bdcdc748da3a7b2bc4c51cb79681b2b72982158
alemiranda7 <7alemiranda@gmail.com>
2022-07-07 16:02:18 -0300

    alteração da pagina de entrada

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import NavBar from "./components/NavBar";
 import logo from './logo.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <>
     <NavBar />
+    <Banner />
     </>
   )
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='containerMSG'>
+        
+        <div className='boxMSG'>
+        <p>É meu primeiro site. Estou usando o VS Code e estudando pelos cursos das plataformas udemy e origamid.</p> 
+        <p>Atualmente buscando o caminho de desenvolvedor full-stack com experiência em Javascript. </p>
+        <p>Este site vai mostrar o meu progesso longo dos cursos e evoluirá para um portfólio responsivo, esteticamente agradável e totalmente construído! =D</p>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
